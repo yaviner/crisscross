3859c1290d4e614d43dbd661ca0b1540422007a5
Yair <yaviner@gmail.com>
2013-04-07 01:07:23 -0400

    bootstrap fixes, locomotivization

--- app/controllers/pages_controller.js
+++ app/controllers/pages_controller.js
@@ -6,6 +6,12 @@ var PagesController = new Controller();
 PagesController.main = function() {
   this.title = 'crisscross'
   this.render();
+  console.log("this is method1");
 }
 
+PagesController.showCrossview = function(){
+	this.title="crisscross"
+	this.render('crossview');
+	console.log("this is method2");
+}
 module.exports = PagesController;
--- config/routes.js
+++ config/routes.js
@@ -7,4 +7,5 @@
 // information.
 module.exports = function routes() {
   this.root('pages#main');
+  this.match('crossview', {controller: 'pages', action: 'showCrossview'});
 }
