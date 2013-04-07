e7b19558fbbd417a6c8ef4edcae4e4f5374d7ee4
Yair <yaviner@gmail.com>
2013-04-07 10:09:31 -0400

    working with nodejitsu

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var locomotive = require('locomotive');
-var config = require('./config/config');
+//var config = require('./config/');
 
 var address = '0.0.0.0';
 console.log("booting");
-locomotive.boot(__dirname, config.env, function(err, server) {
+locomotive.boot(__dirname, 'development', function(err, server) {
 	if (err) { throw err; }
-	server.listen(config.port, address, function() {
+	server.listen(3000, 'localhost', function() {
 	  var addr = this.address();
 	  console.log('listening on %s:%d', addr.address, addr.port);
 	});
